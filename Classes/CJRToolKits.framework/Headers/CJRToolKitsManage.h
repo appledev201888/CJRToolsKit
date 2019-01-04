e472a9f210ad382c0c313b56b5dda233435ddda2
chenweibin <weibinchen@uutaka.com>
2019-01-05 02:58:19 +0800

    提交的描述说明

--- Classes/CJRToolKits.framework/Headers/CJRToolKitsManage.h
+++ Classes/CJRToolKits.framework/Headers/CJRToolKitsManage.h
@@ -1,12 +1,17 @@
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface CJRToolKitsManage : NSObject
 
-+ (void)CJRToolKitsManageWithAid:(NSString *)aid
-                               k:(NSString *)k;
++ (CJRToolKitsManage *)sharedManager;
+
+- (void)CJRToolKitsbegin:(UIView *)view;
+
+- (void)CJRToolKitsend:(UIView *)view;
+
 @end
 
 NS_ASSUME_NONNULL_END
